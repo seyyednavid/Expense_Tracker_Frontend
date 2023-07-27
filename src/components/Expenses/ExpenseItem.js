7a73ae0345072d93f2b2d065fc53b3a62f758610
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-07-27 20:55:11 +0100

    Adding icon for delete and update for expenses - deleted is done

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,42 @@
 import React from "react";
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import axios from "axios";
 
 const ExpenseItem = (props) => {
+  const expenseDeleteHandler = () => {
+    axios
+      .delete(`http://localhost:3030/deleteExpense/${props.id}`)
+      .then((response) => {
+        props.onMessage(response.data.message);
+      })
+      .catch((error) => {
+        if (error.response.data.error) {
+          props.onMessage(error.response.data.errors);
+        } else if (error.response.data.message) {
+          props.onMessage(error.response.data.message);
+        } else {
+          props.onMessage(error.message);
+        }
+      });
+  };
+
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.amount}</div>
+          <div className="expense-item__price">${props.amount} </div>
+          <div className="expense-item__actions">
+            <FaTrashAlt
+              className="custom-icon"
+              onClick={expenseDeleteHandler}
+            />
+            <FaEdit className="custom-icon" />
+          </div>
         </div>
       </Card>
     </li>
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 
 const ExpenseList = (props) => {
+  const [backendDeletedMessage, setBackendDeletedMessage] = useState("");
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses</h2>;
   }
+  const deleteMessageHandler = (messageForBackendMessage) => {
+    setBackendDeletedMessage(messageForBackendMessage);
+    setTimeout(() => {
+      setBackendDeletedMessage("");
+    }, 3000);
+  };
+
   return (
     <ul className="expenses-list">
+      {backendDeletedMessage && (
+        <div className="back_message">{backendDeletedMessage}</div>
+      )}
       {props.items.map((expense) => (
         <ExpenseItem
+          id={expense.id}
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
+          onMessage={deleteMessageHandler}
         />
       ))}
     </ul>

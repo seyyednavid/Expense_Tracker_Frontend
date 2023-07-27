2e594408fc098a948504bd6833a2d8bd74a2d679
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-07-27 10:04:23 +0100

    Using axios for getting data from server instead of static file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
 const App = () => {
-  const [expense, setExpense] = useState(DUMY_EXPENSES);
+  const [expense, setExpense] = useState([]);
+
+  function parseISODate(dateString) {
+    return new Date(dateString);
+  }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/")
+      .then((response) => {
+        // Parse date strings to Date objects
+        const expensesWithDates = response.data.data.map((expense) => ({
+          ...expense,
+          date: parseISODate(expense.date),
+        }));
+        setExpense(expensesWithDates);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  });
 
   const addExpenseData = (expense) => {
     setExpense((prevState) => {
@@ -44,14 +36,6 @@ const App = () => {
       <Expenses items={expense} />
     </div>
   );
-
-  // return (
-  //   React.createElement(
-  //    'div',
-  //     {},
-  //     React.createElement('h2',{}, 'Let\'s get started!')),
-  //     React.createElement(Expenses, {items:expenses})
-  // )
 };
 
 export default App;
